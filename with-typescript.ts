ba81d0e1c969bf1f990a4ffbfe5fae5ff5a224bb
Che Van Thao <chevanthao96@gmail.com>
2022-12-27 13:55:17 +0900

    type Aliases

--- with-typescript.ts
+++ with-typescript.ts
@@ -3,3 +3,14 @@ let course: string | number = 'React - The complete Guide'
 
 course = 12341
 
+// type Aliases 
+
+type Person = {
+    name: string,
+    age: number
+}
+
+const person: Person = {
+    name: 'thao',
+    age: 26
+}
